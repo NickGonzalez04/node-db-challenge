c2f56251b35c129343d9a5cd27abc3f3323d7a8b
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-11 11:18:53 -0400

    Server running

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/', (req,res)=>{
+    res.status(200).json({ message: 'In there like swim wear' });
+})
+
+
+module.exports = server;
